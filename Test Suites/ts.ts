bdd6788c6254510fb4a91aa0ac66a2a5deb014ac
nhivo <nhivo@192.168.1.90>
2021-09-29 12:23:51 +0700

    added

---
+++ Test Suites/ts.ts
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>ts</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>e7829a9c-e107-45d0-a335-9e90567f8b1f</testSuiteGuid>
+</TestSuiteEntity>
